bd0e69bd10e3c31f0794db5eae1aec98f8c3514f
Elif Konak <108223472+elifkk14@users.noreply.github.com>
2025-07-28 21:23:51 +0300

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,7 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3001');
+const socket = io("https://spades-server.vercel.app", {
+  transports: ["websocket"]
+});
 
 export default socket;
