3f6e9a056cb93a1627ef0e0126ce44f484bba277
Elif Konak <108223472+elifkk14@users.noreply.github.com>
2025-07-28 21:36:06 +0300

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://spades-server.vercel.app", {
+const socket = io("https://spades-server.onrender.com", {
   transports: ["websocket"]
 });
 
